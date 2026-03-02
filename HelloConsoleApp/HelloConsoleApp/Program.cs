a309c9ae34a5a4fa98e75a7d569550e67bb82039
Yash <ankit.sodha123@gmail.com>
2026-03-02 11:10:50 +0530

    Day 1 Console Application
    Variable, Data Types, Static, Functions, Scope, Return, Public, Private

---
+++ HelloConsoleApp/HelloConsoleApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var value = 100;
+            string msg = "Hello World!!! Good Morning!!!";
+            Console.WriteLine(msg);
+            //if (value == 100)
+            //{
+            //    Console.WriteLine("Value is 100.");
+            //    return;
+            //}            
+            //Console.WriteLine("Main End.");
+
+            string UserName = Console.ReadLine();
+            Console.WriteLine($"Welcome, {UserName}");
+        }
+    }
+}
