b08bc772d7f251e64ef7997ebca16c3a3376829b
Ankit Sodha <ankit.sodha123@gmail.com>
2026-03-03 09:57:12 +0530

    Array, Pass By Value, Pass By Reference, String methods, Recursion,
    int.Parse vs int.TryParse,
    out vs ref
    Sorting of Int and String

--- HelloConsoleApp/HelloConsoleApp/Program.cs
+++ HelloConsoleApp/HelloConsoleApp/Program.cs
@@ -10,9 +10,9 @@ namespace HelloConsoleApp
     {
         static void Main(string[] args)
         {
-            var value = 100;
-            string msg = "Hello World!!! Good Morning!!!";
-            Console.WriteLine(msg);
+            //var value = 100;
+            //string msg = "Hello World!!! Good Morning!!!";
+            //Console.WriteLine(msg);
             //if (value == 100)
             //{
             //    Console.WriteLine("Value is 100.");
@@ -20,8 +20,50 @@ namespace HelloConsoleApp
             //}            
             //Console.WriteLine("Main End.");
 
-            string UserName = Console.ReadLine();
-            Console.WriteLine($"Welcome, {UserName}");
+            //string UserName = Console.ReadLine();
+            //Console.WriteLine($"Welcome, {UserName}");
+
+            //BooleanComparison();
+
+            //StringIsNullOrEmptyTesting();
+
+            SortingExample.SortNumbersExample();
+        }
+
+        static void BooleanComparison() {
+            bool? IsTest = null;            
+            int num1 = ReadNumber();            
+            int num2 = ReadNumber();
+            int sum = num1 + num2;
+            Console.WriteLine($"{num1} + {num2} = {sum}");            
+        }
+
+        static int ReadNumber() {
+            Console.Write("Enter number : ");
+            //return int.Parse(Console.ReadLine());
+            var input = Console.ReadLine();
+            int number;
+            bool IsParsed = int.TryParse(input, out number);
+            
+            if (IsParsed)
+            {
+                return number;
+            }
+            // Recursion - Function call itself
+            return ReadNumber();              
+        }
+
+        static void StringIsNullOrEmptyTesting()
+        {
+            Console.Write("Enter name : ");            
+            var name = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(name))
+            {
+                StringIsNullOrEmptyTesting();
+                return;
+            }
+            Console.WriteLine($"Welcome, {name}");
         }
     }
 }
---
+++ HelloConsoleApp/HelloConsoleApp/SortingExample.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloConsoleApp
+{
+    public class SortingExample
+    {
+        public static void SortNumbersExample()
+        {
+            int[] numbers = { 4, 12, 3, 6, 1 };
+            PrintData(numbers, "Before Sorting");
+            SortNumbers(numbers);
+            PrintData(numbers, "After Sorting");
+
+            int[] Marks;    // NullReferenceException
+            Marks = new int[10];
+            // Marks[10] -> IndexOutOfRangeException
+
+            int value = 100;
+            Console.WriteLine($"\n\nBefore Change - {value}");
+            PassByValue(value);
+            Console.WriteLine($"After Change - {value}");
+
+            Console.WriteLine($"\n\nReference Before Change - {value}");
+            PassByReferenceWithPrimite(ref value);
+            Console.WriteLine($"Reference After Change - {value}");
+
+            PrintData(numbers, "Before Chaning Array");
+            PassByReference(numbers);
+            PrintData(numbers, "After Changing Array");
+        }
+
+        private static void SortNumbers(int[] numbers) {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    if (numbers[i] > numbers[j])
+                    {
+                        var temp = numbers[i];
+                        numbers[i] = numbers[j];
+                        numbers[j] = temp;
+                    }
+                }
+            }
+        }
+
+        private static void PrintData(int [] numbers, string msg) {
+            var StringMessage = string.Format("\n\n{0}", msg);
+            Console.WriteLine(StringMessage);
+            foreach(var num in numbers) {
+                Console.Write($"{num}, ");
+            }
+        }
+
+        private static void PassByValue(int value) {
+            value += 100;
+        }
+
+        private static void PassByReferenceWithPrimite(ref int value)
+        {
+            value += 100;
+        }
+
+        private static void PassByReference(int[] values)
+        {
+            values[0] += 100;
+        }
+    }
+}
