b68c7fcc17fab27d0de7527731788df1197c25d8
Ankit Sodha <ankit.sodha123@gmail.com>
2026-03-24 10:25:52 +0530

    Create & Edit Category with Serverside and Clientside validation

--- BulkyWeb/BulkyWebApp/Controllers/CategoryController.cs
+++ BulkyWeb/BulkyWebApp/Controllers/CategoryController.cs
@@ -15,8 +15,55 @@ namespace BulkyWebApp.Controllers
 
         public IActionResult Index()
         {
-            List<Category> CategoryList = _db.Categories.ToList();
+            List<Category> CategoryList = _db.Categories.OrderBy(c => c.DisplayOrder).ToList();
             return View(CategoryList);
+        }
+
+        public IActionResult Create()
+        {        
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category Category)
+        {
+            if (Category.Name == Category.DisplayOrder.ToString()) {
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name."); 
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(Category);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Edit(int? Id)
+        {
+            if (Id == null || Id == 0) 
+            {
+                return NotFound();
+            }
+
+            Category? CategoryObj = _db.Categories.Find(Id);
+
+            if (CategoryObj == null) {
+                return NotFound();
+            }
+
+            return View(CategoryObj);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category Category)
+        {           
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(Category);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
         }
     }
 }
